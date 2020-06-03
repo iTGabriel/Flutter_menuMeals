fee7639a8ec251665903b7143e3cec00cbdd0605
itGabriel <gabriel.gustavo.s@hotmail.com>
2020-06-03 02:30:39 -0300

    updating

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -6,7 +6,7 @@ class MealItem extends StatelessWidget {
 
   const MealItem(this.meal);
 
-  void _selectMeal(){}
+  void _selectMeal() {}
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +14,7 @@ class MealItem extends StatelessWidget {
       onTap: () => _selectMeal,
       child: Card(
         shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(15),
-          topRight: Radius.circular(15),
-        )),
+            borderRadius: BorderRadius.all(Radius.circular(15))),
         margin: const EdgeInsets.all(10),
         elevation: 5,
         child: Column(
@@ -34,7 +31,45 @@ class MealItem extends StatelessWidget {
                     height: 250,
                     width: double.infinity,
                     fit: BoxFit.cover,
-                    )
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    right: 5,
+                    child: Container(
+                      width: 300,
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      child: Text(meal.title, 
+                      style: Theme.of(context).textTheme.headline6.apply(fontSizeDelta: 5, backgroundColor: Color.fromRGBO(0, 0, 0, 0.3)), 
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  
+                  Row(children: <Widget>[
+                      Icon(Icons.access_time),
+                      Text("${meal.duration.toString()} min")
+                    ],
+                  ),
+
+                  Row(children: <Widget>[
+                      Icon(Icons.work),
+                      Text(" ${meal.mealComplexity}"),
+                    ],
+                  ),
+
+                  Row(children: <Widget>[
+                      Icon(Icons.attach_money),
+                      Text(" ${meal.MealCost}")
+                  ])
                 ],
               ),
             )
